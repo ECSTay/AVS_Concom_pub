ed93ae25f8ab55219ca28b504cad4ab58f944415
ETay <evelyn.tay@sydney.edu.au>
2025-07-08 14:17:39 +0800

    changes 8 July

--- Concom_data_cleaning.R
+++ Concom_data_cleaning.R
@@ -5,46 +5,25 @@ library(stringr)
 library(data.table)
 library(here)
 
-readRenviron(here(".Renviron"))
-RDS_PATH <- Sys.getenv("RDS_PATH")
-
-read_rds(RDS_PATH, file = "/Analyses/Concomitant vaccination/Infant_NIP_MenB_concom_modelling.rda")
-
-load("Z:/Analyses/Concomitant vaccination/Infant_NIP_MenB_concom_modelling.rda")
+load("Z:/Analyses/Concomitant vaccination/Infant_NIP_MenB_concom_modelling.rda")#connect to USyd RDS)
 str(infant)
 
 dat <- as.data.table(infant)
 colnames(dat) <- tolower(colnames(dat))
-dat[is.na(dat)] <- 0
-dat <- dat[!(is.na(any_event)) &
-                           !(is.na(atsi)) &
-                           !(is.na(sex)) & 
-                           !(is.na(group)) &
-                           !(is.na(pmh)), ]
-
-
-
 
 table(dat$group)
 #Concomitant vaccination    Seperate vaccination 
 #8749                    2352 
-dat$group <- str_replace_all(dat$group, c("Concomitant vaccination" = "1", "Separate vaccination" = "2"))
+dat$group <- str_replace_all(dat$group, c("Concomitant vaccination" = "1", "Seperate vaccination" = "2"))
+dat$group <- as.integer(dat$group)
+dat$schedule <- str_replace_all(dat$schedule, c("2 months" = "1", "4 months" = "2",
+                                                "6 months" = "3", "12 months" = "4"))
+dat$schedule <- as.integer(dat$schedule)
 dat$sex <- str_replace_all(dat$sex, c("Female" = "1", "Male" = "0"))
+dat$sex <- as.numeric(dat$sex)
 setnames(dat, "atsi", "indig")
-dat$schedule <- str_replace_all(dat$schedule, c("2 mths" = "1", "4 mths" = "2",
-                                                 "6 mths" = "3", "12 mths" = "4"))
+dat$any_event <- as.integer(dat$any_event)
 
 
 
-
-
-#?changing to factors
-dat[,
-           `:=`(
-             sex = factor(sex, levels = c("1", "2")), #Female = 1
-             indig = factor(indig, levels = c("1", "2")),#Non_aboriginal = 1
-             group = factor(group, levels = c("1", "2")),#Concom = 1, Separate = 2, 
-             schedule = as.factor(schedule)
-             
-           )]
 
