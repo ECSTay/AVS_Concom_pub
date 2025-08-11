9b020b873c921ca2324c305d587a0fa327d4c848
ETay <evelyn.tay@sydney.edu.au>
2025-08-11 16:01:39 +0800

    Update

--- Model_C_posterior_draft.R
+++ Model_C_posterior_draft.R
@@ -5,7 +5,7 @@ library(ggplot2)
 
 
 #load in data
-#dat <- read.csv(file = "C:/Users/ETay/Documents/Work documents/AVS work/Thuy_concom/AVS_Concom_pub/dat_modelC.csv")
+dat <- read.csv(file = "C:/Users/ETay/Documents/Work documents/AVS work/Thuy_concom/AVS_Concom_pub/dat_modelC.csv")
 
 N_C = nrow(dat)                   ## number of responders
 N_strat_C  = 2                    ## number of strategies
@@ -16,18 +16,18 @@ s_C <- dat$vax_sequence          ## vaccine strategy, 1 = "Concomitant vaccinati
 t_C <- dat$sched                  ## schedule - 1 = 2 months, 2 = 4 months, 3 = 6 months, 4 = 12 months
 w_C <- dat$sex                    ## sex - 0 = "Male", 1 = "Female"
 x_C <- dat$indig                  ## Indigenous status -0 = Non-indig, 1 = Aboriginal and Torres Strait Islander
-z_C <- dat$pmh                    ## comorbidity
+z_C <- dat$pmh                    ## comorbidity - 0 - None, 1 - at least one
 
-y <- dat$any_event + 1        ## outcome - 0,1,2 
-y <- dat$impact + 1           ## outcome - 0,1,2
-y <- dat$ma + 1               ## outcome - 0,1,2
+y_C <- dat$any_event + 1      ## outcome - 0,1,2 p(1) = 0.49, p(2) = 0.03
+y_C <- dat$impact + 1           ## outcome - 0,1,2 p(1) = 0.04, p(2) = 0.0005
+y_C <- dat$ma + 1               ## outcome - 0,1,2 p(1) = 0.02
 
-a <- qlogis(0.3)             ## prior distribution mean for one event - depends on the schedule and the vaccine strategy?
+a <- qlogis(0.49)             ## prior distribution mean for one event - depends on the schedule and the vaccine strategy
 b <- 1                       ## prior distribution standard deviation for two events
-c <- qlogis(0.05)            ## prior distribution mean for two events
+c <- qlogis(0.03)            ## prior distribution mean for two events
 d <- 1                       ## prior distribution for standard deviation for two events
 
-  dat_C <- list(N = N_C,
+dat_C <- list(N = N_C,
                 N_strat = N_strat_C,
                 N_sched = N_sched_C,
                 s = s_C,
@@ -43,17 +43,19 @@ d <- 1                       ## prior distribution for standard deviation for tw
 
 
 ##SAP code
-model_C <- cmdstan_model("C:/Users/etay/Documents/Work documents/AVS work/Thuy_concom/model_C.stan")
+model_C <- cmdstan_model("C:/Users/etay/Documents/Work documents/AVS work/Thuy_concom/AVS_Concom_pub/model_C.stan")
 
 fit_C <- model_C$sample(dat_C, chains = 8, parallel_chains = 8) #16 minutes with 100 warmups and 200 draws
 draws_full <- as_draws_matrix(fit_C$draws(c("mu", "beta", "gamma", "delta")))
 saveRDS(draws_full, "draws_full_any_event.rds")
+
 draws_C <- as_draws_matrix(fit_C$draws(c("mu")))
 dat_vis_C <- data.frame(x = plogis(as.vector(draws_C)),
                         strategy = rep(rep(c("Concomitant", "Separate"), each = 8000), 4*2),
                         schedule = rep(c("2 months", "4 months", "6 months", "12 months"), each = 8000*2),
                         Parameter = rep(c("P(k = 1)", "P(k = 2)"), each = 8000*2*4))
 dat_vis_C <- dat_vis_C[!(dat_vis_C$strategy == "Concomitant" & dat_vis_C$Parameter == "P(k = 2)"),]
+
 
 ggplot(dat_vis_C, aes(x = x, colour = Parameter)) +
   facet_grid(schedule~strategy) +
