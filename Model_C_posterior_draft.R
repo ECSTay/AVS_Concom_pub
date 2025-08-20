0f7aaf537b69448dd5f3bcf045a47b7a8f098fdf
ETay <evelyn.tay@sydney.edu.au>
2025-08-20 15:49:41 +0800

    Update

--- Model_C_posterior_draft.R
+++ Model_C_posterior_draft.R
@@ -87,73 +87,84 @@ for(strat in 1:2){
 }
 
 dat_vis <- data.table(x = plogis(as.vector(draws_marg)),
+                      samp = rep(1:8000,2*4*2),
                       strategy = rep(rep(c("Concomitant", "Separate"), each = 8000), 4*2),
                       schedule = rep(rep(c("2 months", "4 months", "6 months", "12 months"), each = 8000*2), 2),
                       Parameter = rep(c("P(k = 1)", "P(k = 2)"), each = 8000*2*4))
 dat_vis <- dat_vis[!(strategy == "Concomitant" & Parameter == "P(k = 2)")]
 
+dat_vis <- dcast.data.table(dat_vis, samp + strategy + schedule ~ Parameter, value.var = 'x')
+#dat_vis[, 'P(k >= 1)' := 'P(k = 1)' + 'P(k = 2)']
+dat_vis$"P(k >= 1)" <- dat_vis$'P(k = 1)' + dat_vis$'P(k = 2)'
+dat_vis <- melt.data.table(dat_vis, id.vars = c('samp', 'strategy', 'schedule'), measure.vars = c('P(k = 1)', 'P(k = 2)', 'P(k >= 1)'), variable.name = 'Parameter', value.name = 'x')
+dat_vis <- dat_vis[!(strategy == 'Concomitant' & Parameter %in% c('P(k = 2)', 'P(k >= 1)'))]
+
+dat_vis <- dat_vis[!(strategy == 'Separate' & Parameter %in% c('P(k = 1)', 'P(k = 2)'))]
+
+
 #AEFI
 ggplot(dat_vis, aes(x = x, colour = Parameter)) +
-  facet_grid(forcats::fct_relevel(schedule, "2 months", "4 months", "6 months", "12 months")~strategy) +
+  facet_grid(strategy~forcats::fct_relevel(schedule, "2 months", "4 months", "6 months", "12 months")) +
   geom_density() +
   xlab("Probability") +
   ylab("Density") +
   labs(color = NULL) +
   theme(legend.position = "bottom") +
-  scale_colour_manual(values = c("P(k = 1)" = "#D55E00", "P(k = 2)" = "#009E73"),
-                      labels = c("One report of AEFI", "Two reports of AEFI"))
+  scale_colour_manual(values = c("P(k = 1)" = "#D55E00", "P(k >= 1)" = "#009E73"),
+                      labels = c("One report of AEFI", "At least one report of AEFI"))
 
-ggsave("AEFI_2.png", dpi = 400, width = 6, height = 5, units = "in")
+ggsave("AEFI_3.png", dpi = 400, width = 6, height = 5, units = "in")
 
 #local reaction
 
 ggplot(dat_vis, aes(x = x, colour = Parameter)) +
-  facet_grid(forcats::fct_relevel(schedule, "2 months", "4 months", "6 months", "12 months")~strategy) +
+  facet_grid(strategy~forcats::fct_relevel(schedule, "2 months", "4 months", "6 months", "12 months")) +
   geom_density() +
   xlab("Probability") +
   ylab("Density") +
   labs(color = NULL) +
   theme(legend.position = "bottom") +
-  scale_colour_manual(values = c("P(k = 1)" = "#D55E00", "P(k = 2)" = "#009E73"),
-                      labels = c("One report of local reaction", "Two reports of local reactions"))
+  scale_colour_manual(values = c("P(k = 1)" = "#D55E00", "P(k >= 1)" = "#009E73"),
+                      labels = c("One report of local reaction", "At least one report of local reaction"))
 
-ggsave("Local_2.png", dpi = 400, width = 6, height = 5, units = "in")
+
+ggsave("Local_3.png", dpi = 400, width = 6, height = 5, units = "in")
 #fever
 ggplot(dat_vis, aes(x = x, colour = Parameter)) +
-  facet_grid(forcats::fct_relevel(schedule, "2 months", "4 months", "6 months", "12 months")~strategy) +
+  facet_grid(strategy~forcats::fct_relevel(schedule, "2 months", "4 months", "6 months", "12 months")) +
   geom_density() +
   xlab("Probability") +
   ylab("Density") +
   labs(color = NULL) +
   theme(legend.position = "bottom") +
-  scale_colour_manual(values = c("P(k = 1)" = "#D55E00", "P(k = 2)" = "#009E73"),
-                      labels = c("One report of fever", "Two reports of fever"))
+  scale_colour_manual(values = c("P(k = 1)" = "#D55E00", "P(k >= 1)" = "#009E73"),
+                      labels = c("One report of fever", "At least one report of fever"))
 
-ggsave("Fever_2.png", dpi = 400, width = 6, height = 5, units = "in")
+ggsave("Fever_3.png", dpi = 400, width = 6, height = 5, units = "in")
 #MA
 ggplot(dat_vis, aes(x = x, colour = Parameter)) +
-  facet_grid(forcats::fct_relevel(schedule, "2 months", "4 months", "6 months", "12 months")~strategy) +
+  facet_grid(strategy~forcats::fct_relevel(schedule, "2 months", "4 months", "6 months", "12 months")) +
   geom_density() +
   xlab("Probability") +
   ylab("Density") +
   labs(color = NULL) +
   theme(legend.position = "bottom") +
-  scale_colour_manual(values = c("P(k = 1)" = "#D55E00", "P(k = 2)" = "#009E73"),
-                      labels = c("One report of MA", "Two reports of MA"))
+  scale_colour_manual(values = c("P(k = 1)" = "#D55E00", "P(k >= 1)" = "#009E73"),
+                      labels = c("One report of MA", "At least one report of MA"))
 
-ggsave("MA_2.png", dpi = 400, width = 6, height = 5, units = "in")
+ggsave("MA_3.png", dpi = 400, width = 6, height = 5, units = "in")
 #impact
 ggplot(dat_vis, aes(x = x, colour = Parameter)) +
-  facet_grid(forcats::fct_relevel(schedule, "2 months", "4 months", "6 months", "12 months")~strategy) +
+  facet_grid(strategy~forcats::fct_relevel(schedule, "2 months", "4 months", "6 months", "12 months")) +
   geom_density() +
   xlab("Probability") +
   ylab("Density") +
   labs(color = NULL) +
   theme(legend.position = "bottom") +
-  scale_colour_manual(values = c("P(k = 1)" = "#D55E00", "P(k = 2)" = "#009E73"),
-                      labels = c("One report of carer impact", "Two reports of carer impact"))
+  scale_colour_manual(values = c("P(k = 1)" = "#D55E00", "P(k >= 1)" = "#009E73"),
+                      labels = c("One report of carer impact", "At least one report of carer impact"))
 
-ggsave("Impact_2.png", dpi = 400, width = 6, height = 5, units = "in")
+ggsave("Impact_3.png", dpi = 400, width = 6, height = 5, units = "in")
 
 ## probabilities and 95% credible intervals by schedule
 
@@ -207,3 +218,28 @@ hist(plogis(rnorm(10000, -4, 1)))
 hist(plogis(rnorm(10000, -1, 1)))
 hist(plogis(rnorm(10000, -1, 0.5)))
 
+twomonths <- dat_vis[strategy == "Separate" & schedule == "2 months"]
+one <- twomonths[Parameter == "P(k = 1)"]$x
+two <- twomonths[Parameter == "P(k = 2)"]$x
+atleastone <- one + two
+
+fourmonths <- dat_vis[strategy == "Separate" & schedule == "4 months"]
+one_4 <- fourmonths[Parameter == "P(k = 1)"]$x
+two_4 <- fourmonths[Parameter == "P(k = 2)"]$x
+atleastone_4 <- one_4 + two_4
+
+sixmonths <- dat_vis[strategy == "Separate" & schedule == "6 months"]
+one_6 <- sixmonths[Parameter == "P(k = 1)"]$x
+two_6 <- sixmonths[Parameter == "P(k = 2)"]$x
+atleastone_6 <- one_6 + two_6
+
+twelvemonths <- dat_vis[strategy == "Separate" & schedule == "12 months"]
+one_12 <- twelvemonths[Parameter == "P(k = 1)"]$x
+two_12 <- twelvemonths[Parameter == "P(k = 2)"]$x
+atleastone_12 <- one_12 + two_12
+
+atleastone <- c(atleastone, atleastone_4, atleastone_6, atleastone_12)
+strategy <- rep("Separate",32000)
+schedule <- c(rep("2 months",8000), rep("4 months", 8000), rep("6 months", 8000), rep ("12 months", 8000))
+Parameter <- rep("P(>= 1)")
+dat_vis_sub <- data.frame(x = atleastone, )
