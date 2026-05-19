425be2703f308e8dfc035b904cd4760d6d254996
Evelyn Tay <evelyn.tay@telethonkids.org.au>
2026-05-19 11:22:41 +0800

    19/05/2026

--- Extracting_from_posteriors.R
+++ Extracting_from_posteriors.R
@@ -1,126 +1,96 @@
 ###Visualising and tabulating the posteriors - marginalised estimates
 library(data.table)
 library(ggplot2)
+library(ggdist)
+library(distributional)
 library(tidyverse)
 library(stringr)
 library(posterior)
-postr <- readRDS(file = "C:/Users/ETay/Documents/Concom_AEFI_posterior_SA_eps.rds")
-postr <- posterior::as_draws_matrix(fit_C$draws())    
-postr <- posterior::as_draws_matrix(postr) 
-
 
 #load in the relevant posterior
 draws <- readRDS(file ="C:/Users/ETay/Documents/postr_concom_AEFI.rds")
+#draws <- readRDS(file ="C:/Users/ETay/Documents/postr_concom_ma_SA.rds")# SA with N(0),10) priors
 
-dat_C <- readRDS("C:/Users/ETay/Documents/dat_C_AEFI.rds")
+#load in STAN dat
 
-#load in dat
-dat <- fread(file = "C:/Users/ETay/Documents/NIP_MenB_dat.csv")
-dat$res <- as.integer(factor(dat$uid_person, levels = unique(dat$uid_person)))
-dat$vax_sequence[dat$vax_sequence == "2"] <- 0
-dat$vax_sequence <- as.integer(dat$vax_sequence)
-
-
+dat <- readRDS("C:/Users/ETay/Documents/dat_C_AEFI.rds")
 #marginalise - using option 2.	The event probability (e.g., one/two MAs) for an average person 
 #from the population of survey responders receiving their X month schedule.
 
-draws_marg <- array(NA, dim = c(nrow(draws), 2, 4, 2), 
-                    dimnames = list(draw = 1:nrow(draws), 
-                                    strategy = c("concom", "separate"), 
-                                    schedule = c("2 months", "4 months", "6 months", "12 months"), 
-                                    parameter = c("one", "two")))
-sex_weights <- dat[, .(mn = mean(sex)), by = schedule]
-indig_weights <- dat[, .(mn = mean(indig)), by = schedule]
-state_weights <- data.table(mn = as.vector(prop.table(table(dat$schedule,dat$clinic_state), margin = 1)[,-2]),
-                            schedule = rep(1:4, 7),
-                            state = rep(1:7, each = 4))
-clinic_weights <- data.table(mn = as.vector(prop.table(table(dat$schedule,dat$clinic_type), margin = 1)[,-2]),
-                             schedule = rep(1:4, 2),
-                             state = rep(1:2, each = 4))
-comorb_weights <- dat[, .(mn = mean(pmh)), by = schedule]
-
-
-for(strat in 1:2){
-  for(sched in 1:4){
-    for(par in 1:2){
-      mu_par <- draws[,paste0("mu[", sched, ",", par, "]")]
-      alpha_par <- (strat == 2)*draws[,paste0("alpha[", sched, "]")]
-      sex_par <- draws[,paste0("beta[", par, "]")]*sex_weights[schedule == sched]$mn
-      indig_par <- draws[,paste0("gamma[", par, "]")]*indig_weights[schedule == sched]$mn
-      state_par <- as.vector(state_weights[schedule==sched]$mn%*%t(draws[,paste0("rho[", 1:7, ",", par, "]")]))
-      clinic_par <- as.vector(clinic_weights[schedule==sched]$mn%*%t(draws[,paste0("tau[", 1:2, ",", par, "]")]))
-      comorb_par <- draws[,paste0("delta[", par, "]")]*comorb_weights[schedule == sched]$mn
-      if(strat == 2 & par == 2){
-        draws_marg[, strat, sched, par] <- -Inf
-      } else {
-        draws_marg[, strat, sched, par] <- as.vector(mu_par + alpha_par + sex_par + indig_par + state_par + clinic_par + comorb_par)  
-      }
+ndraws <- nrow(draws)
+p_post <- array(data = NA, dim = c(ndraws, 3, dat$N_R))
+for(r in 1:dat$N_R){
+    mu1 <- draws[1:ndraws,paste0("mu[", dat$t[r], ",1]")]
+    mu2 <- draws[1:ndraws,paste0("mu[", dat$t[r], ",2]")]
+    alpha <- draws[1:ndraws,paste0("alpha[", dat$t[r], "]")]
+    epsilon1 <- rnorm(ndraws, sd = draws[1:ndraws,"sigma_epsilon[1]"])
+    epsilon2 <- rnorm(ndraws, sd = draws[1:ndraws,"sigma_epsilon[2]"])
+    beta1 <- draws[1:ndraws,"beta[1]"]
+    beta2 <- draws[1:ndraws,"beta[2]"]
+    gamma1 <- draws[1:ndraws,"gamma[1]"]
+    gamma2 <- draws[1:ndraws,"gamma[2]"]
+    rho1 <- draws[1:ndraws,paste0("rho[", 1:dat$N_state, ",1]")]
+    rho2 <- draws[1:ndraws,paste0("rho[", 1:dat$N_state, ",2]")]
+    tau1 <- draws[1:ndraws,paste0("tau[", 1:dat$N_clinic, ",1]")]
+    tau2 <- draws[1:ndraws,paste0("tau[", 1:dat$N_clinic, ",2]")]
+    delta1 <- draws[1:ndraws,"delta[1]"]
+    delta2 <- draws[1:ndraws,"delta[2]"]
+    eta <- array(NA, dim = c(ndraws, 3))
+    eta[,1] <- 0
+    eta[,2] <- mu1 + dat$s[r]*alpha + epsilon1 + dat$w[r]*beta1 + dat$x[r]*gamma1 + as.vector(dat$q[r,]%*%t(rho1)) + as.vector(dat$c[r,]%*%t(tau1)) + dat$z[r]*delta1
+    if(dat$s[r] == 0){
+      eta[,3] <- mu2 + epsilon2 + dat$w[r]*beta2 + dat$x[r]*gamma2 + as.vector(dat$q[r,]%*%t(rho2)) + as.vector(dat$c[r,]%*%t(tau2)) + dat$z[r]*delta2
+    } else {
+      eta[,3] <- - Inf
     }
-  }
+    p_post[,,r] <- exp(eta - apply(eta, 1, function(x) max(x) + log(sum(exp(x - max(x))))))
+    
 }
+  
+dat_vis <- data.table(sim = rep(1:ndraws, 3*dat$N_R),#can add eg sex
+                       p   = as.vector(p_post),
+                       Outcome = rep(rep(c("0", "1", "2"), each = ndraws), dat$N_R),
+                       s   = factor(rep(dat$s, each = ndraws*3), labels = c("Separate", "Concomitant")),
+                       t   = factor(rep(dat$t, each = ndraws*3), labels = c("2 months", "4 months", "6 months", "12 months")))
+  dat_vis <- dat_vis[Outcome != "0", .(p_marg = mean(p)), by = .(sim, Outcome, s, t)]##avge over the covariates
+  dat_vis <- dcast.data.table(dat_vis, sim + s + t ~ Outcome, value.var = "p_marg")
+  dat_vis[, `1+` := `1` + `2`]
+  dat_vis <- melt.data.table(dat_vis, id.vars = c("sim", "s", "t"), measure.vars = c("1", "2", "1+"), variable.name = "Outcome", value.name = "p_marg")
+  dat_vis[, Outcome := factor(Outcome, levels = c("1", "2", "1+"), labels = c("P(1 event)", "P(2 events)", "P(at least one event)"))]
+  
+  ggplot(dat_vis[Outcome == "P(at least one event)"], aes(x = p_marg, colour = s)) +
+    facet_grid(s~forcats::fct_relevel(t, "2 months", "4 months", "6 months", "12 months")) +
+    geom_density() +
+    xlab("Probability") +
+    ylab("Density") +
+    labs(color = NULL) +
+    theme(legend.position = "bottom") +
+    scale_colour_manual(values = c("Concomitant" = "#D55E00", "Separate" = "#009E73"),
+                        labels = c("One report of AEFI", "At least one report of AEFI"))
+  
+  summ_f <- function(x) paste0(round(median(x), 2), " (", round(quantile(x, 0.025), 2), ",", round(quantile(x, 0.975), 2), ")")
+  dat_vis[, summ_f(p_marg), by = .(Outcome, s, t)]
 
-dat_vis <- data.table(x = as.vector(draws_marg),
-                      samp = rep(1:8000,2*4*2),
-                      strategy = rep(rep(c("Separate", "Concomitant"), each = 8000), 4*2),
-                      schedule = rep(rep(c("2 months", "4 months", "6 months", "12 months"), each = 8000*2), 2),
-                      Parameter = rep(c("eta_1", "eta_2"), each = 8000*2*4))
-                      
-                      #Parameter = rep(c("P(k = 1)", "P(k = 2)"), each = 8000*2*4))
-dat_vis <- dcast.data.table(dat_vis, samp + strategy + schedule ~ Parameter, value.var = 'x')
+  dat_vis_cont <- dcast.data.table(dat_vis[Outcome == "P(at least one event)"], sim + t ~ s, value.var = "p_marg")
+  dat_vis_cont[, contrast := Concomitant - Separate] 
+  
+  ggplot(dat_vis_cont, aes(x = contrast)) +
+    facet_grid(~forcats::fct_relevel(t, "2 months", "4 months", "6 months", "12 months")~.) +
+    geom_density() +
+    geom_vline(xintercept = 0, linetype = "dashed") +
+    xlab("Change in Probability") +
+    ylab("Density")
+  
+  dat_vis_cont[, summ_f(contrast), by = t]
 
+########################################################################
 
-eta <- array(NA, dim = c(ndraws, 3))
-eta[,1] <- 0
-eta[,2] <- mu1 + dat$s[r]*alpha + epsilon1 + dat$w[r]*beta1 + dat$x[r]*gamma1 + as.vector(dat$q[r,]%*%t(rho1)) + as.vector(dat$c[r,]%*%t(tau1)) + dat$z[r]*delta1
-if(dat$s[r] == 0){
-  eta[,3] <- mu2 + epsilon2 + dat$w[r]*beta2 + dat$x[r]*gamma2 + as.vector(dat$q[r,]%*%t(rho2)) + as.vector(dat$c[r,]%*%t(tau2)) + dat$z[r]*delta2
-} else {
-  eta[,3] <- - Inf
-}
-
-dat_vis[, `:=`(
-  p1 = exp(eta[,2] - apply(eta, 1, function(x) max(x) + log(sum(exp(x - max(x)))))),
-  p2 = exp(eta[,3] - apply(eta, 1, function(x) max(x) + log(sum(exp(x - max(x))))))
-)]
-dat_vis[, p12 := p1 + p2]
-dat_vis[,.(mean(p1),mean(p2)), by = .(strategy, schedule)][order(schedule)]
-dat_vis <- melt.data.table(dat_vis, id.vars = c('samp', 'strategy', 'schedule'), measure.vars = "p12", variable.name = 'Parameter', value.name = 'x')
-#dat_vis <- melt.data.table(dat_vis, id.vars = c('samp', 'strategy', 'schedule'), measure.vars = c('P(k = 1)', 'P(k = 2)', 'P(k >= 1)'), variable.name = 'Parameter', value.name = 'x')
-#dat_vis <- dat_vis[!(strategy == 'Concomitant' & Parameter %in% c('P(k = 2)', 'P(k >= 1)'))]
-#dat_vis <- dat_vis[!(strategy == 'Separate' & Parameter %in% c('P(k = 1)', 'P(k = 2)'))]
-
-dat_vis[strategy == "Separate", mean(x), by = .(schedule, Parameter)]
-dat_vis[strategy == "Concomitant", mean(x), by = .(schedule, Parameter)]
-
-#contrasts - separate strategy at  least one event postr - concomitant 1 event postr 
-#then summarise as another column in Tabble 3
-
-#AEFI
-ggplot(dat_vis, aes(x = x, colour = strategy)) +
-  facet_grid(strategy~forcats::fct_relevel(schedule, "2 months", "4 months", "6 months", "12 months")) +
-  geom_density() +
-  xlab("Probability") +
-  ylab("Density") +
-  labs(color = NULL) +
-  theme(legend.position = "bottom") +
-  scale_colour_manual(values = c("Concomitant" = "#D55E00", "Separate" = "#009E73"),
-                      labels = c("One report of AEFI", "At least one report of AEFI"))
-
-# ggplot(dat_vis, aes(x = x, colour = Parameter)) +
-#   facet_grid(strategy~forcats::fct_relevel(schedule, "2 months", "4 months", "6 months", "12 months")) +
-#   geom_density() +
-#   xlab("Probability") +
-#   ylab("Density") +
-#   labs(color = NULL) +
-#   theme(legend.position = "bottom") +
-#   scale_colour_manual(values = c("P(k = 1)" = "#D55E00", "P(k >= 1)" = "#009E73"),
-#                       labels = c("One report of AEFI", "At least one report of AEFI"))
 
 ggsave("AEFI_3.png", dpi = 400, width = 6, height = 5, units = "in")
 
-## Figure 3 code
 
-## probabilities and 95% credible intervals by schedule
+
+## probabilities and 95% credible intervals by schedule for Table 3 - add a contrast column
 
 summary_fun_C <- function(strat, sch, dat_vis){
   if(strat == "Concomitant"){
@@ -155,18 +125,5 @@ for (sch in schedules) {
 print(results1)
 print(results2)
 
-
-
-sched <- 3 # 6 months
-par <- 2 
-strat <- 1
-      mu_par <- draws[,paste0("mu[", sched, ",", par, "]")]
-       alpha_par <- (strat == 2)*draws[,paste0("alpha[", sched, "]")]
-       sex_par <- draws[,paste0("beta[", par, "]")]*sex_weights[schedule == sched]$mn
-       indig_par <- draws[,paste0("gamma[", par, "]")]*indig_weights[schedule == sched]$mn
-       state_par <- as.vector(state_weights[schedule==sched]$mn%*%t(draws[,paste0("rho[", 1:7, ",", par, "]")]))
-       clinic_par <- as.vector(clinic_weights[schedule==sched]$mn%*%t(draws[,paste0("tau[", 1:2, ",", par, "]")]))
-       comorb_par <- draws[,paste0("delta[", par, "]")]*comorb_weights[schedule == sched]$mn
- summary(mu_par)
 
 
