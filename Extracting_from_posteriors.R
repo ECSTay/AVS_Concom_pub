6fdc5f03ff1d025f1f30aff6f7439cc39b000213
Evelyn Tay <evelyn.tay@telethonkids.org.au>
2026-05-19 16:37:35 +0800

    19/05/2026

--- Extracting_from_posteriors.R
+++ Extracting_from_posteriors.R
@@ -18,47 +18,50 @@ dat <- readRDS("C:/Users/ETay/Documents/dat_C_AEFI.rds")
 #from the population of survey responders receiving their X month schedule.
 
 ndraws <- nrow(draws)
-p_post <- array(data = NA, dim = c(ndraws, 3, dat$N_R))
-for(r in 1:dat$N_R){
-    mu1 <- draws[1:ndraws,paste0("mu[", dat$t[r], ",1]")]
-    mu2 <- draws[1:ndraws,paste0("mu[", dat$t[r], ",2]")]
-    alpha <- draws[1:ndraws,paste0("alpha[", dat$t[r], "]")]
-    epsilon1 <- rnorm(ndraws, sd = draws[1:ndraws,"sigma_epsilon[1]"])
-    epsilon2 <- rnorm(ndraws, sd = draws[1:ndraws,"sigma_epsilon[2]"])
-    beta1 <- draws[1:ndraws,"beta[1]"]
-    beta2 <- draws[1:ndraws,"beta[2]"]
-    gamma1 <- draws[1:ndraws,"gamma[1]"]
-    gamma2 <- draws[1:ndraws,"gamma[2]"]
-    rho1 <- draws[1:ndraws,paste0("rho[", 1:dat$N_state, ",1]")]
-    rho2 <- draws[1:ndraws,paste0("rho[", 1:dat$N_state, ",2]")]
-    tau1 <- draws[1:ndraws,paste0("tau[", 1:dat$N_clinic, ",1]")]
-    tau2 <- draws[1:ndraws,paste0("tau[", 1:dat$N_clinic, ",2]")]
-    delta1 <- draws[1:ndraws,"delta[1]"]
-    delta2 <- draws[1:ndraws,"delta[2]"]
-    eta <- array(NA, dim = c(ndraws, 3))
-    eta[,1] <- 0
-    eta[,2] <- mu1 + dat$s[r]*alpha + epsilon1 + dat$w[r]*beta1 + dat$x[r]*gamma1 + as.vector(dat$q[r,]%*%t(rho1)) + as.vector(dat$c[r,]%*%t(tau1)) + dat$z[r]*delta1
-    if(dat$s[r] == 0){
-      eta[,3] <- mu2 + epsilon2 + dat$w[r]*beta2 + dat$x[r]*gamma2 + as.vector(dat$q[r,]%*%t(rho2)) + as.vector(dat$c[r,]%*%t(tau2)) + dat$z[r]*delta2
-    } else {
-      eta[,3] <- - Inf
+ninds <- 1000
+p_post <- array(data = NA, dim = c(ndraws, 4, 2, 4))
+for(t in 1:4){
+  inds <- sample(which(dat$t == t), size = ninds, replace = TRUE)
+  mu1 <- draws[1:ndraws,paste0("mu[", t, ",1]")]
+  mu2 <- draws[1:ndraws,paste0("mu[", t, ",2]")]
+  alpha <- draws[1:ndraws,paste0("alpha[", t, "]")]
+  epsilon1 <- rnorm(ndraws, sd = draws[1:ndraws,"sigma_epsilon[1]"])
+  epsilon2 <- rnorm(ndraws, sd = draws[1:ndraws,"sigma_epsilon[2]"])
+  beta1 <- draws[1:ndraws,"beta[1]"]
+  beta2 <- draws[1:ndraws,"beta[2]"]
+  gamma1 <- draws[1:ndraws,"gamma[1]"]
+  gamma2 <- draws[1:ndraws,"gamma[2]"]
+  rho1 <- draws[1:ndraws,paste0("rho[", 1:dat$N_state, ",1]")]
+  rho2 <- draws[1:ndraws,paste0("rho[", 1:dat$N_state, ",2]")]
+  tau1 <- draws[1:ndraws,paste0("tau[", 1:dat$N_clinic, ",1]")]
+  tau2 <- draws[1:ndraws,paste0("tau[", 1:dat$N_clinic, ",2]")]
+  delta1 <- draws[1:ndraws,"delta[1]"]
+  delta2 <- draws[1:ndraws,"delta[2]"]
+  for(s in 1:2){
+    p_tmp <- array(NA, dim = c(ndraws, 4, ninds))
+    for(r in 1:ninds){
+      eta <- array(NA, dim = c(ndraws, 3))
+      eta[,1] <- 0
+      eta[,2] <- mu1 + (s == 2)*alpha + epsilon1 + dat$w[inds[r]]*beta1 + dat$x[inds[r]]*gamma1 + as.vector(dat$q[inds[r],]%*%t(rho1)) + as.vector(dat$c[inds[r],]%*%t(tau1)) + dat$z[inds[r]]*delta1
+      if(s == 1){
+        eta[,3] <- mu2 + epsilon2 + dat$w[inds[r]]*beta2 + dat$x[inds[r]]*gamma2 + as.vector(dat$q[inds[r],]%*%t(rho2)) + as.vector(dat$c[inds[r],]%*%t(tau2)) + dat$z[inds[r]]*delta2
+      } else {
+        eta[,3] <- - Inf
+      }
+      p_tmp[,1:3,r] <- exp(eta - apply(eta, 1, function(x) max(x) + log(sum(exp(x - max(x))))))
+      p_tmp[,4,r] <- p_tmp[,2,r] + p_tmp[,3,r]
     }
-    p_post[,,r] <- exp(eta - apply(eta, 1, function(x) max(x) + log(sum(exp(x - max(x))))))
-    
+    p_post[,,s,t] <- apply(p_tmp, c(1,2), mean)
+  }
 }
   
-dat_vis <- data.table(sim = rep(1:ndraws, 3*dat$N_R),#can add eg sex
-                       p   = as.vector(p_post),
-                       Outcome = rep(rep(c("0", "1", "2"), each = ndraws), dat$N_R),
-                       s   = factor(rep(dat$s, each = ndraws*3), labels = c("Separate", "Concomitant")),
-                       t   = factor(rep(dat$t, each = ndraws*3), labels = c("2 months", "4 months", "6 months", "12 months")))
-  dat_vis <- dat_vis[Outcome != "0", .(p_marg = mean(p)), by = .(sim, Outcome, s, t)]##avge over the covariates
-  dat_vis <- dcast.data.table(dat_vis, sim + s + t ~ Outcome, value.var = "p_marg")
-  dat_vis[, `1+` := `1` + `2`]
-  dat_vis <- melt.data.table(dat_vis, id.vars = c("sim", "s", "t"), measure.vars = c("1", "2", "1+"), variable.name = "Outcome", value.name = "p_marg")
-  dat_vis[, Outcome := factor(Outcome, levels = c("1", "2", "1+"), labels = c("P(1 event)", "P(2 events)", "P(at least one event)"))]
+dat_vis <- data.table(sim = rep(1:ndraws, 4*2*4),
+                      p_marg   = as.vector(p_post),
+                      Outcome = factor(rep(rep(c("P(0 events)", "P(1 event)", "P(2 events)", "P(at least one event)"), each = ndraws), 2*4), levels = c("P(0 events)", "P(1 event)", "P(2 events)", "P(at least one event)")),
+                      s   = factor(rep(rep(c("Separate", "Concomitant"), each = ndraws*4), 4), levels = c("Separate", "Concomitant")),
+                      t   = factor(rep(c("2 months", "4 months", "6 months", "12 months"), each = ndraws*4*2), levels = c("2 months", "4 months", "6 months", "12 months")))
   
-  ggplot(dat_vis[Outcome == "P(at least one event)"], aes(x = p_marg, colour = s)) +
+ggplot(dat_vis[Outcome == "P(at least one event)"], aes(x = p_marg, colour = s)) +
     facet_grid(s~forcats::fct_relevel(t, "2 months", "4 months", "6 months", "12 months")) +
     geom_density() +
     xlab("Probability") +
